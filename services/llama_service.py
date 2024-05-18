13cd87d7974e2138c1d0aebdb3a1ca19893f9808
Kazakov Mihail <advancerman@gmail.com>
2024-05-18 17:24:09 +0300

    Add exercise-assessment cycle

--- services/llama_service.py
+++ services/llama_service.py
@@ -2,7 +2,9 @@ from llamaapi import LlamaAPI
 from langchain_experimental.llms import ChatLlamaAPI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
-from utils.prompts import EXERCISE_PROMPT, QUESTION_ANSWER_PROMPT, SUMMARIZATION_PROMPT, THEORY_GENERATION_PROMPT
+from utils.prompts import EXERCISE_PROMPT, QUESTION_ANSWER_PROMPT, SUMMARIZATION_PROMPT, THEORY_GENERATION_PROMPT, \
+    ASSESSMENT_PROMPT
+
 
 class LlamaService:
     def __init__(self, api_token, temperature=1):
@@ -11,18 +13,16 @@ class LlamaService:
         self.output_parser = StrOutputParser()
         self.chain = self.chat_model | self.output_parser
 
-    def generate_exercise(self, user_prompt):
-        """
-        Generate an English exercise based on the user prompt.
-        
-        Args:
-            user_prompt (str): The user's prompt containing exercise information.
-        
-        Returns:
-            str: The generated English exercise.
-        """
+    def generate_exercise(self, topic):
         prompt_template = PromptTemplate.from_template(EXERCISE_PROMPT)
-        prompt = prompt_template.format(exercise_goal=user_prompt)
+        prompt = prompt_template.format(topic_name=topic[1])
+        return self.chain.invoke(prompt)
+
+    def assess_exercise_answer(self, topic, generated_exercise, user_answer):
+        prompt_template = PromptTemplate.from_template(ASSESSMENT_PROMPT)
+        prompt = prompt_template.format(topic_name=topic[1],
+                                        generated_exercise=generated_exercise,
+                                        user_answer=user_answer)
         return self.chain.invoke(prompt)
 
     def answer_question(self, user_question):
@@ -63,7 +63,7 @@ class LlamaService:
         Returns:
             str: The generated grammar theory card.
         """
-        
+
         prompt_template = PromptTemplate.from_template(THEORY_GENERATION_PROMPT)
         prompt = prompt_template.format(theory_topic=theory_topic)
         return self.chain.invoke(prompt)
--- utils/prompts.py
+++ utils/prompts.py
@@ -1,5 +1,26 @@
 EXERCISE_PROMPT = """
-You are a professional English native teacher with a TEFL certificate. Your goal is to create high-quality English exercises for language learners. The exercises should be engaging, educational, and tailored to the user's needs. Use emojis sometimes. Do not say hi, Do not include greetings, information about yourself etc in exercise text!!!. Just generated exercise without any additional text like 'Here is your exercies', 'good luck' etc. Format of the generated exercise: 'Exercise_topic: ... . NEWLINE Exersice: ... '. Exercise Goal: {exercise_goal}.
+You are a professional English native teacher with a TEFL certificate.
+Your goal is to create high-quality English exercises for language learners.
+The exercises should be engaging, educational, and tailored to the user's needs.
+Use emojis sometimes.
+Do not say hi, Do not include greetings, information about yourself etc in exercise text!!!.
+Just generated exercise without any additional text like 'Here is your exercies', 'good luck' etc.
+Format of the generated exercise: 'Exercise_topic: ... . NEWLINE Exersice: ... '.
+Exercise Topic: {topic_name}.
+"""
+
+ASSESSMENT_PROMPT = """
+You are a professional English native teacher with a TEFL certificate.
+Your goal is to assess answers English exercise for a language learner.
+Use emojis sometimes.
+Do not say hi, Do not include greetings, information about yourself etc in assessment text!!!.
+Just assess exercise without any additional text like 'Here is your assessment', 'good luck' etc.
+Format of the generated assessment: 'Exercise_topic: ... . NEWLINE Mistakes: mistakes_description NEWLINE Received points: correct_answers / exercises_count '.
+Exercise Topic: {topic_name}.
+English exercise:
+{generated_exercise}.
+Answers to assess:
+{user_answer}.
 """
 
 QUESTION_ANSWER_PROMPT = """
